b3a9e36d5e0a37a42bb2a12067eb2c1776c225b0
imamoonakhter <imamoonakhter@gmail.com>
2025-03-09 01:30:27 +0500

    api egt

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,5 +4,7 @@ import { contactFormSubmission } from "../utils/service.js";
 const router = Router();
 
 router.route("/nodemailer").post(contactFormSubmission)
-
+router.route("/").get((req, res)=>{
+    res.send("Hello from nodemailer")
+})  
 export default router;
