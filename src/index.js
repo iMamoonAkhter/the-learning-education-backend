81c4463af8f1ecabd6225d3cc4a200957826b5b8
imamoonakhter <imamoonakhter@gmail.com>
2025-03-09 01:01:15 +0500

    port uppercase

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ dotenv.config({
 })
 
 
-    app.listen(process.env.port || 8000, ()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
         console.log(`Server is running at port : ${process.env.PORT || 8000}`)
     })
 
