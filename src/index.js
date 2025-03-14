78f9e61df85507e87a376e1a4778a8c42b97469f
imamoonakhter <imamoonakhter@gmail.com>
2025-03-14 16:00:40 +0500

    some changes

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@
  dotenv.config();
 
 
-     app.listen(process.env.PORT || 8000, ()=>{
-         console.log(`Server is running at port : ${process.env.PORT || 8000}`)
+     app.listen(process.env.PORT || 3000, ()=>{
+         console.log(`Server is running at port : ${process.env.PORT || 3000}`)
      })
 
 
