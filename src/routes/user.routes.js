09e260c88199b0e9527f4f41f9fa1c5595939f62
imamoonakhter <imamoonakhter@gmail.com>
2025-03-14 16:36:37 +0500

    change

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,7 +4,5 @@ import { contactFormSubmission } from "../utils/service.js";
 const router = Router();
 
 router.route("/nodemailer").post(contactFormSubmission)
-router.route("/").get((req, res)=>{
-    res.send("Hello from nodemailer")
-})  
+
 export default router;
